1d53b548c7a5543cda7ba9aaef8a35385e105c84
Pélé Oussoumanou <43408405+othimar@users.noreply.github.com>
2022-12-04 11:13:35 +0100

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val data: ArrayList<ArrayList<Int>> = ArrayList()
+        data.add(ArrayList())
+        for (str in input) {
+            if (str.isBlank()) {
+                data.add(ArrayList())
+            } else {
+                data.last().add(str.toInt())
+            }
+        }
+        return data.map { it.sum() }.max()
+
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val data: ArrayList<ArrayList<Int>> = ArrayList()
+        data.add(ArrayList())
+        for (str in input) {
+            if (str.isBlank()) {
+                data.add(ArrayList())
+            } else {
+                data.last().add(str.toInt())
+            }
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val dataSum = data.map { it.sum() }
+        return with(dataSum.sorted()) {
+            val first = this.last()
+            val second = this[dataSum.size - 2]
+            val third = this[dataSum.size - 3]
+
+            first + second + third
+        }
+
+    }
+    check(part1(readInput("Day01_test")) == 24000)
+    check(part2(readInput("Day01_test")) == 45000)
+
 
     val input = readInput("Day01")
     println(part1(input))
